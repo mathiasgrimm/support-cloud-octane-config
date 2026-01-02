2b127499b5790d866b5c0879a6e45854b076434d
Mathias Grimm <mathiasgrimm@gmail.com>
2026-01-02 15:22:05 -0300

    octane

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-1', function () {
+    $privateDisk = array_filter((array) config('filesystems.disks.private'));
+
+    if ($privateDisk) {
+        //logger("disk found");
+    } else {
+        logger("disk not found");
+    }
+});
